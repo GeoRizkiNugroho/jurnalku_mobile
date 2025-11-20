7c9e38c2b6cbc3b65b214596b65da6738245c4b5
GeoRizkiNugroho <geosuryani@gmail.com>
2025-11-20 12:35:21 +0700

    Progress Page

--- lib/progress_page.dart
+++ lib/progress_page.dart
@@ -5,10 +5,6 @@ class ProgressPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Progress Page"),
-      ),
-    )
+    return Scaffold(body: Center(child: Text("Progress Page")));
   }
 }
