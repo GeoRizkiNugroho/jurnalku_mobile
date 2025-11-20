6244504b1df56be371de432ef094f1e766404376
grigndiisyu <[negarigendisayuyudhani@smkwikrama.sch.id]>
2025-11-20 11:19:44 +0700

    ,

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Jurnalku Mobile',
-      debugShowCheckedModeBanner: false,
       home: LoginPage(),
     );
   }
