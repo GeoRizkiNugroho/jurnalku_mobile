ad8f153c411a148a148c25840b4a7659109cf5b8
GeoRizkiNugroho <geosuryani@gmail.com>
2025-12-04 12:46:35 +0700

    login

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Jurnalku Mobile',
-      home: JurnalPembiasaanPage()
+      home: LoginPage()
     );
   }
 }
