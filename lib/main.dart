dece66d45a0fe61ff854ff8dbe9830b9d18ae78b
GeoRizkiNugroho <geosuryani@gmail.com>
2025-12-05 09:27:04 +0700

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:jurnalku_mobile/dashboard_page.dart';
-import 'package:jurnalku_mobile/jelajahi_page.dart';
-import 'package:jurnalku_mobile/tabeloptimasi_page.dart';
 import 'login_page.dart';
 
 void main() {
